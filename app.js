d7c77aa74a20f7f2de8149f513b0ff136dbd120e
iccee0 <github.r@12i.ch>
2020-12-09 16:21:25 +0100

    pls

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//MongoDB stuf
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.DB_CONNECTION_STRING;
 // create an express app
 const express = require("express")
 const app = express()
@@ -9,6 +12,22 @@ app.use(express.static("public"))
 app.get("/", function(req, res) {
     res.send("<h1>Hello World!</h1>")
 })
+
+
+//getLists
+MongoClient.connect(url, function(err, db) {
+    if (err)
+        throw err;
+    var dbo = db.db("mydb");
+    var mysort = { name: 1 };
+    dbo.collection("customers").find().sort(mysort).toArray(function(err, result) {
+        if (err)
+            throw err;
+        lists = result;
+        console.log(result);
+        db.close();
+    });
+});
 
 
 //list to api
