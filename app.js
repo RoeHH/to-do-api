21b261fb94afe5aa80505116dc05a946b00231d4
iccee0 <github.r@12i.ch>
2020-12-09 16:09:23 +0100

    t

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ require("dotenv").config();
 var MongoClient = require('mongodb').MongoClient;
 var url = process.env.DB_CONNECTION_STRING;
 // create an express app
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 var bodyParser = require('body-parser');
 // use the express-static middleware
 app.use(express.static("public"));
 // define the first route
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
     res.send("<h1>Hello World!</h1>");
 });
 //getLists
-MongoClient.connect(url, function (err, db) {
+MongoClient.connect(url, function(err, db) {
     if (err)
         throw err;
     var dbo = db.db("mydb");
     var mysort = { name: 1 };
-    dbo.collection("customers").find().sort(mysort).toArray(function (err, result) {
+    dbo.collection("customers").find().sort(mysort).toArray(function(err, result) {
         if (err)
             throw err;
         lists = result;
@@ -27,8 +27,8 @@ MongoClient.connect(url, function (err, db) {
     });
 });
 //list to api
-app.get('/lists', function (req, res) {
+app.get('/lists', function(req, res) {
     res.json(result);
 });
 // start the server listening for requests
-app.listen(process.env.PORT || 3000, function () { return console.log("Server is running on Port:" + process.env.PORT || 3000); });
+app.listen(process.env.PORT || 3000, function() { return console.log("Server is running on Port:" + process.env.PORT || 3000); });
