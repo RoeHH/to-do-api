7d5b9a9b01c120849a5bbdba8d7b03b5c2f8bc2b
iccee0 <github.r@12i.ch>
2020-12-09 16:14:51 +0100

    d

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+//MongoDB stuf
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.DB_CONNECTION_STRING;
 // create an express app
 const express = require("express")
 const app = express()
@@ -6,9 +10,29 @@ const app = express()
 app.use(express.static("public"))
 
 // define the first route
-app.get("/p", function(req, res) {
+app.get("/", function(req, res) {
     res.send("<h1>Hello World!</h1>")
 })
+
+//getLists
+MongoClient.connect(url, function(err, db) {
+    if (err)
+        throw err;
+    var dbo = db.db("mydb");
+    var mysort = { name: 1 };
+    dbo.collection("customers").find().sort(mysort).toArray(function(err, result) {
+        if (err)
+            throw err;
+        lists = result;
+        console.log(result);
+        db.close();
+    });
+});
+
+//list to api
+app.get('/lists', function(req, res) {
+    res.json(result);
+});
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000,
