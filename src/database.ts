69b3f5acdcdb065c57826ad8394dbc11cb6c1bd1
iccee0 <github.r@12i.ch>
2020-12-10 12:52:54 +0100

    api check

--- src/database.ts
+++ src/database.ts
@@ -1,51 +1,57 @@
 import { Collection, Db, MongoClient, ObjectId } from 'mongodb';
 
 export class DbController {
+  private dbo?: Db;
+  private customers?: Collection;
+  private list?: Collection;
+  private client?: MongoClient;
+  private task?: Collection;
 
-    private dbo?: Db;
-    private customers?: Collection
-    private list?: Collection
-    private client?: MongoClient
-    private task?: Collection
+  private constructor() { }
 
-    private constructor() { }
+  public static createDbController(): DbController {
+    const controller = new DbController();
 
-    public static async createDbController(): Promise<DbController> {
-        const controller = new DbController();
+    controller.client = new MongoClient(process.env.DB_CONNECTION_STRING || '');
+    controller.client.connect().then(() => {
+      controller.dbo = controller.client?.db();
+      controller.list = controller.dbo?.collection('lists');
+      controller.task = controller.dbo?.collection('tasks');
+    });
 
-        controller.client = new MongoClient(process.env.DB_CONNECTION_STRING || '');
-        await controller.client.connect()
-        controller.dbo = controller.client.db();
-        controller.customers = controller.dbo.collection('customers');
-        controller.list = controller.dbo.collection('lists');
-        controller.task = controller.dbo.collection('tasks');
+    return controller;
+  }
+
+  public async getLists(): Promise<any[] | undefined> {
+    const lists = await this.list?.find().toArray();
+    if (!lists) return undefined; // If collection is empty
+    for (const list of lists) list.tasks = await this.task?.find({ listid: list._id }).toArray();
+    return lists;
+  }
 
 
-        return controller;
-    }
+  public async insertTodo(listid: number, taskContent: string /*, userId: ObjectId*/): Promise<void> {
+    this.task?.insertOne({ taskContent, listid: new ObjectId(listid) });
+  }
 
-    public async getTodos(): Promise<any[] | undefined> {
-        return this.customers?.find().toArray();
-    }
+  public async newList(listName: string): Promise<void> {
+    this.list?.insertOne({ listName, archived: false });
+  }
 
-    public async getLists(): Promise<any[] | undefined> {
-        const lists = await this.list?.find().toArray();
-        if (!lists) return undefined; // If collection is empty
-        for (const list of lists)
-            list.tasks = await this.task?.find({ listid: list._id }).toArray();
-        return lists;
-    }
-
-    public async getCustomerByName(name: string) {
-        const customer = await this.customers?.findOne({ name }) as any;
-        return customer._id;
-    }
-
-    public async insertTodo(listid: number, taskContent: string/*, userId: ObjectId*/): Promise<void> {
-        this.task?.insertOne({ taskContent, listid: new ObjectId(listid) });
-    }
-
-    public async newList(listName: string): Promise<void> {
-        this.list?.insertOne({ listName });
-    }
+  public async updateTask(taskid: number, done: boolean): Promise<void> {
+    this.task?.updateOne(
+      { _id: new ObjectId(taskid) },
+      {
+        $set: { done },
+      }
+    );
+  }
+  public async updateList(listid: number, archived: boolean): Promise<void> {
+    this.list?.updateOne(
+      { _id: new ObjectId(listid) },
+      {
+        $set: { archived },
+      }
+    );
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { DbController } from './database';
+const controller = DbController.createDbController();
 import bodyParser from 'body-parser';
 
 // create an express app
 import { join } from 'path';
-import express from "express";
+import express from 'express';
 const app = express();
 
 // use the express-static middleware
@@ -14,42 +15,43 @@ app.use(express.static(join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//list to api
-app.get('/customers', async (req, res) => {
-    const controller = await DbController.createDbController();
-    const customers = await controller.getTodos();
-    res.status(200).json(customers);
-});
 
-app.get('/name', async (req, res) => {
-    const controller = await DbController.createDbController();
-    const customer = await controller.getCustomerByName('Company Inc');
-    res.status(200).json(customer);
+app.get('/lists', async (req, res) => {
+  const customers = await controller.getLists();
+  res.status(200).json(customers);
 });
 
 app.post('/new-list', async (req, res) => {
-    const list = req.body;
-    console.log(list);
-    const controller = await DbController.createDbController();
-    controller.newList(list.listName);
-    res.send('List is added to the database');
+  const list = req.body;
+  console.log(list);
+  await controller.newList(list.listName);
+  res.redirect('/lists');
 });
 
 app.post('/new-task', async (req, res) => {
-    const task = req.body;
-    console.log(task);
-    const controller = await DbController.createDbController();
-    controller.insertTodo(task.listid, task.taskContent);
-    res.send('List is added to the database');
+  const task = req.body;
+  console.log(task);
+  await controller.insertTodo(task.listid, task.taskContent);
+  res.redirect('/lists');
 });
 
-app.get('/lists', async (req, res) => {
-    const controller = await DbController.createDbController();
-    const customers = await controller.getLists();
-    res.status(200).json(customers);
+app.post('/update-task', async (req, res) => {
+  const taskid = req.body.taskid;
+  const done = Boolean(req.body.done);
+  await controller.updateTask(taskid, done);
+  res.redirect('/lists');
+});
+
+app.post('/update-list', async (req, res) => {
+  const listid = req.body.listid;
+  console.log(listid);
+  const archived = Boolean(req.body.archived);
+  console.log(req.body.archived);
+  await controller.updateList(listid, archived);
+  res.redirect('/lists');
 });
 
 // start the server listening for requestsy
 app.listen(process.env.PORT || 3000, () =>
-    console.log("Server is running on Port:" + process.env.PORT || 3000)
+  console.log('Server is running on Port:' + process.env.PORT || 3000)
 );
