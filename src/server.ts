ec5118529987250e3ffc946f2ee7ad09774c9eef
iccee0 <github.r@12i.ch>
2021-01-06 03:55:32 +0100

    CORS 2 header for the same thing?

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,8 @@ app.use(express.static(join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'http://localhost');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8100');// update to match the domain you will make the request from
   res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
