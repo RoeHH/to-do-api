65897c1db2914ea3985f454df3c551982698c673
iccee0 <github.r@12i.ch>
2021-01-08 15:08:33 +0100

    Wildcarded ACAO

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ app.use(express.static(join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost');// update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*');// update to match the domain you will make the request from
   res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
